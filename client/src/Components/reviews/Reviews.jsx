885ac79ec9202d52010572f9ad8fb8531dec8f7c
NaN-niki <niki_stst@abv.bg>
2023-11-29 21:24:46 +0200

    Reviews page first commit

--- client/src/Components/reviews/Reviews.jsx
+++ client/src/Components/reviews/Reviews.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import * as reviewService from '../../services/reviewsServices'
+import styles from './Reviews.module.css'
 
 function Reviews() {
     const [reviews, setReviews] = useState([])
@@ -9,12 +10,18 @@ function Reviews() {
     }, [])
 
     console.log(reviews)
-    
+
     return (
-        <section>
+        <section className={styles.container}>
             <div className="content-container">
                 <h1>All Reviews</h1>
-
+                {reviews.map(r => (
+                    <div key={r._id} >
+                        <p>Title: {r.title}</p>
+                        <p>IMAGE: !!!</p>
+                        <p>Description: {r.description}</p>
+                    </div>
+                ))}
             </div>
         </section>
     )
