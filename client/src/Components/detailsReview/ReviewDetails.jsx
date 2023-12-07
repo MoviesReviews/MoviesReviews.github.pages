23ef961497844943a0d3a4fb6c62a7e18d762d4b
NaN-niki <niki_stst@abv.bg>
2023-12-07 17:09:54 +0200

    small changes

--- client/src/Components/detailsReview/ReviewDetails.jsx
+++ client/src/Components/detailsReview/ReviewDetails.jsx
@@ -35,7 +35,7 @@ function ReviewDetails() {
                     </div>
                 </div>
 
-                <Comments/>
+                {/* <Comments/> */}
             </div>
         </section>
     )
--- client/src/Components/detailsReview/comments/Comments.jsx
+++ client/src/Components/detailsReview/comments/Comments.jsx
@@ -1,5 +1,20 @@
-export default function Comments(){
-    return{
-       
+import { useForm } from "../../../hooks/useForm"
+
+export function Comments() {
+    const { formValues, onSubmit, onChange } = useForm(submitComment, {
+        comment: ''
+    })
+    const submitComment = (value) => {
+        console.log(value)
     }
+    return (
+        <div className="">
+            <h2>Comments: </h2>
+            <h3>Write a comment: </h3>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="comment"></label>
+                <input type="text" id="comment" name="comment" value={formValues.comment} onChange={onChange} />
+            </form>
+        </div>
+    )
 }
