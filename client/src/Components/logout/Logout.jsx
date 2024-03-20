b7c36d77db2a3df53cbe9bf9d9821463595af684
NaN-niki <niki_stst@abv.bg>
2024-03-20 12:40:37 +0200

    Logout Loading component imported

--- client/src/Components/logout/Logout.jsx
+++ client/src/Components/logout/Logout.jsx
@@ -1,18 +1,26 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { logout } from "../../services/userService";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/authContext";
+import { LoadComponent } from "../loadComponent/LoadComponent";
 
 export function Logout() {
     const { logoutHandler } = useContext(AuthContext)
+    const [isLoading, setIsLoading] = useState(false)
     const navigate = useNavigate()
 
     useEffect(() => {
+        setIsLoading(true)
         logout()
             .then(() => {
+                setIsLoading(true)
                 logoutHandler()
                 navigate('/')
             })
             .catch(err => console.log(err))
-    }, [])
+    }, []) 
+    
+    if(isLoading){
+        return <LoadComponent/>
+    }
 }
